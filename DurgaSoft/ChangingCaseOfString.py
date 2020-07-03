b33857befb3d7d7f8775a6ce14e0133683e89c92
YPvenkat <vyalagala@techforce.ai>
2020-07-03 16:57:47 +0530

    initial commit

---
+++ DurgaSoft/ChangingCaseOfString.py
@@ -0,0 +1,21 @@
+"""
+1. upper()===>To convert all characters to upper case
+2. lower() ===>To convert all characters to lower case
+3. swapcase()===>converts all lower case characters to upper case and all upper case characters to
+lower case
+4. title() ===>To convert all character to title case. i.e first character in every word should be upper
+case and all remaining characters should be in lower case.
+5. capitalize() ==>Only first character will be converted to upper case and all remaining characters
+can be converted to lower case.
+
+"""
+
+s='learning Python is very Easy'
+print(s.upper())
+print(s.lower())
+print(s.swapcase())
+print(s.title())
+print(s.capitalize())
+
+
+
